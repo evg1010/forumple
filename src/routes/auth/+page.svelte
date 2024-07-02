f2b904d0a3695020802f91cbffbd67898fdbd8dc
evg1010 <vazquezgarciaeva10@gmail.com>
2024-07-02 06:27:43 -0400

    style: update login page styling and form layout

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -13,10 +13,10 @@
 	class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-fuchsia-500 via-rose-400 to-pink-200"
 >
 	<form
-		class="w-full max-w-md p-8 m-8 space-y-4 rounded-md shadow-md bg-white"
+		class="w-full max-w-md p-8 m-8 space-y-4 rounded-md shadow-2xl bg-white"
 		on:submit|preventDefault={handleLogin}
 	>
-		<h2 class="text-2xl font-semibold text-center text-gray-700">Login</h2>
+		<h2 class="text-3xl font-semibold text-center text-pink-600">Login</h2>
 		<div class="space-y-2">
 			<Label for="username">Username</Label>
 			<Input id="username" type="text" bind:value={username} placeholder="Enter your username" />

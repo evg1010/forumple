fa733f8b2b680fbbb5ad416b0fb4663c9624e094
Eva Ercilia Vázquez García <vazquezgarciaeva10@gmail.com>
2024-07-05 12:36:48 -0400

    Revert "chore: Update primary color shades in tailwind.config.js"
    This reverts commit a4abe09f83e5ddc4e91ee28b727f60cf844eeaf2.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,7 @@ export default {
     extend: {
       colors: {
         // flowbite-svelte
-        primary: {
-          50: '#FFF1F2',
-          100: '#FFE4E6',
-          200: '#FECDD3',
-          300: '#FDA4AF',
-          400: '#FB7185',
-          500: '#F43F6E',
-          600: '#E11D48',
-          700: '#BE123C',
-          800: '#9F1239',
-          900: '#881337'
-        },
+        primary: { 50: '#FFF5F2', 100: '#FFF1EE', 200: '#FFE4DE', 300: '#FFD5CC', 400: '#FFBCAD', 500: '#FE795D', 600: '#EF562F', 700: '#EB4F27', 800: '#CC4522', 900: '#A5371B' },
       }
     },
   },

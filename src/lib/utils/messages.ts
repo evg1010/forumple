4ae5aebd1c2964770febdd21d59ec0a854169d22
evg1010 <vazquezgarciaeva10@gmail.com>
2024-09-23 06:46:02 -0400

    feat: implement real-time messaging and notifications
    • Set up Supabase subscription for real-time updates
    • Handle INSERT, UPDATE, and DELETE events for messages
    • Parse messages with a new utility function
    • Update notifications button with new handleNotificationsOnClick function
    • Refactor messagesData to use Svelte store and parseMessage function

---
+++ src/lib/utils/messages.ts
@@ -0,0 +1,24 @@
+import type { Tables } from '$lib/types/supabase';
+import type { Message } from '$lib/types/types';
+
+export function parseMessage(
+	message: Tables<'messages'>,
+	threadMessages: Tables<'messages'>[],
+	threadUsers: any[]
+): Message {
+	let replyMessageContent = null;
+	if (message.reply_message_id) {
+		replyMessageContent = threadMessages.find(
+			(replyMessage) => replyMessage.id === message.reply_message_id
+		)?.content;
+	}
+
+	return {
+		id: message.id,
+		createdAt: message.created_at,
+		content: message.content,
+		reactions: message.reactions,
+		replyMessageContent: replyMessageContent ?? null,
+		user: threadUsers.find((user) => user?.id === message.user_id)
+	};
+}

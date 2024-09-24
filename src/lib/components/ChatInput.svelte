7dde55a93e600327fe7b78d9a25e13151307c635
evg1010 <vazquezgarciaeva10@gmail.com>
2024-09-24 00:32:34 -0400

    Add reply functionality to chat messages
    - Modified ChatMessage component to dispatch a 'reply' event when the reply button is clicked
    - Updated +page.svelte to handle the 'reply' event from ChatMessage
    - Added reply_message variable to store the message being replied to
    - Passed reply_message to ChatInput component for further handling
    
    These changes enable users to initiate a reply to a specific message in the chat interface.

--- src/lib/components/ChatInput.svelte
+++ src/lib/components/ChatInput.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { Button } from 'flowbite-svelte';
-	import { ArrowUpOutline, PaperClipOutline } from 'flowbite-svelte-icons';
+	import { ArrowUpOutline, CloseCircleSolid } from 'flowbite-svelte-icons';
 	import AttachImage from './AttachImage.svelte';
+	import type { Message } from '$lib/types/types';
 
+	export let reply_message: Message | null = null;
 	let message = '';
 	let textareaElement: HTMLTextAreaElement | null = null;
 
@@ -46,30 +48,42 @@
 	// Watch the message variable and adjust the textarea height accordingly
 	$: autoResize();
 
-	function handleAttachment() {
-		// Implement attachment functionality
-		console.log('Attachment button clicked');
+	function handleCloseReply() {
+		reply_message = null;
 	}
 </script>
 
-<form
-	on:submit|preventDefault={handleSubmit}
-	class="flex gap-2 px-5 py-3 w-full items-center border-t-2"
->
-	<div class="flex flex-grow items-end">
-		<textarea
-			bind:value={message}
-			bind:this={textareaElement}
-			on:input={autoResize}
-			on:keydown={handleKeydown}
-			placeholder="Type your message here ..."
-			rows="1"
-			class="w-full text-sm px-3 p-2 pr-10 rounded-3xl resize-none overflow-y-auto border-gray-200 scrollbar-none"
-			style="line-height: 1.5;"
-		></textarea>
-	</div>
-	<AttachImage />
-	<Button type="submit" class="!p-2" pill={true} color="alternative" disabled={!message.trim()}>
-		<ArrowUpOutline />
-	</Button>
-</form>
+<div class="flex flex-col border-t-2">
+	{#if reply_message}
+		<div class="flex justify-between items-center p-2 mt-2 mx-6 bg-gray-100 rounded-lg">
+			<div class="flex gap-1">
+				<div class="bg-gray-300 w-1.5"></div>
+				<p class="text-gray-600 break-words">
+					{reply_message.content}
+				</p>
+			</div>
+			<Button color="none" pill={true} size="xs" class="!p-2" on:click={handleCloseReply}>
+				<CloseCircleSolid class="w-4 h-4" color="gray" />
+			</Button>
+		</div>
+	{/if}
+
+	<form on:submit|preventDefault={handleSubmit} class="flex gap-2 px-5 py-3 w-full items-center">
+		<div class="flex flex-grow items-end">
+			<textarea
+				bind:value={message}
+				bind:this={textareaElement}
+				on:input={autoResize}
+				on:keydown={handleKeydown}
+				placeholder="Type your message here ..."
+				rows="1"
+				class="w-full text-sm px-3 p-2 pr-10 rounded-3xl resize-none overflow-y-auto border-gray-200 scrollbar-none"
+				style="line-height: 1.5;"
+			></textarea>
+		</div>
+		<AttachImage />
+		<Button type="submit" class="!p-2" pill={true} color="alternative" disabled={!message.trim()}>
+			<ArrowUpOutline />
+		</Button>
+	</form>
+</div>
